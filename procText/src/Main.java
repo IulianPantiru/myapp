6da01556ca645c0c211c121d87362a7cc11656e7
student <student@Debian.myguest.virtualbox.org>
2026-02-23 20:24:25 +0200

    Mesaj descriptiv pentru modificarea ta

--- procText/src/Main.java
+++ procText/src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            String text = Files.readString(Paths.get("input.txt"));
+            String text = Files.readString(Paths.get("/home/student/myapp/input.txt"));
             text = text.replaceAll("[^a-zA-Z0-9\\s]", "");
             text = text.replaceAll("\\s+", " ");
             text = text.toLowerCase();
